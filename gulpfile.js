1df4e01a5a6b9a57f697f106380cfbecdb90194b
Ricardo Casares <ricardocasares@gmail.com>
2014-03-07 19:30:28 -0300

    fixed gulp crashes

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,26 @@ var coffee = require('gulp-coffee');
 var stylus = require('gulp-stylus');
 var concat = require('gulp-concat');
 var jade = require('gulp-jade');
+var notify = require('gulp-notify');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
 var serve = require('gulp-serve');
 var bower = require('gulp-bower');
 
 var paths = {
-  coffee: ['src/**/*.coffee'],
-  styls: ['src/styls/*.styl'],
-  jade: ['src/**/*.jade']
+  coffee: {
+    src: ['src/**/*.coffee'],
+    dst: './build'
+  },
+  stylus: {
+    src: ['src/styls/*.styl'],
+    dst: './build/css'
+  },
+  jade: {
+    src: ['src/**/*.jade'],
+    dst: './build'
+  }
 };
 
 gulp.task('bower', function() {
@@ -21,26 +33,36 @@ gulp.task('bower', function() {
 });
 
 gulp.task('scripts', function() {
-  return gulp.src(paths.coffee)
-    .pipe(coffee())
-    .pipe(gulp.dest('./build'))
+  return gulp.src(paths.coffee.src)
+    .pipe(plumber())
+    .pipe(coffee().on('error', notify.onError(function(error) {
+      return error;
+    })))
+    .pipe(changed(paths.coffee.dst))
+    .pipe(gulp.dest(paths.coffee.dst))
     .pipe(connect.reload());
 });
 
 gulp.task('jade', function() {
-  gulp.src(paths.jade)
-    .pipe(jade())
-    .pipe(gulp.dest('./build'))
+  gulp.src(paths.jade.src)
+    .pipe(jade().on('error', notify.onError(function(error) {
+      return error;
+    })))
+    .pipe(changed(paths.jade.dst))
+    .pipe(gulp.dest(paths.jade.dst))
     .pipe(connect.reload());
 });
 
 // Get and render all .styl files recursively 
 gulp.task('stylus', function() {
-  gulp.src(paths.styls)
+  gulp.src(paths.stylus.src)
     .pipe(stylus({
       set: ['compress']
-    }))
-    .pipe(gulp.dest('./build/css'))
+    }).on('error', notify.onError(function(error) {
+      return error;
+    })))
+    .pipe(changed(paths.stylus.dst))
+    .pipe(gulp.dest(paths.stylus.dst))
     .pipe(connect.reload());
 });
 
@@ -52,9 +74,9 @@ gulp.task('connect', connect.server({
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.coffee, ['scripts']);
-  gulp.watch(paths.styls, ['stylus']);
-  gulp.watch(paths.jade, ['jade']);
+  gulp.watch(paths.coffee.src, ['scripts']);
+  gulp.watch(paths.stylus.src, ['stylus']);
+  gulp.watch(paths.jade.src, ['jade']);
 });
 
 // The default task (called when you run `gulp` from cli)
